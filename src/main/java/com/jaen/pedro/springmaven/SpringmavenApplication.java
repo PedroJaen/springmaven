e714c99453d94fabeeef7c826de66975bcb0487c
Pedro Jaen <pedro.jaenvilla@gmail.com>
2021-04-09 13:35:16 +0200

    first commit

---
+++ src/main/java/com/jaen/pedro/springmaven/SpringmavenApplication.java
@@ -0,0 +1,13 @@
+package com.jaen.pedro.springmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmavenApplication.class, args);
+	}
+
+}
